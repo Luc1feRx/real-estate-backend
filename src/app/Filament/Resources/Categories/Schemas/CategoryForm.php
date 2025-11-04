1d44934d7dd32f53d3889d3be6a2e80bb93089fe
TuPhan2112 <tu.pa@beetechsoft.vn>
2025-11-04 13:31:17 +0700

    feature/category

--- src/app/Filament/Resources/Categories/Schemas/CategoryForm.php
+++ src/app/Filament/Resources/Categories/Schemas/CategoryForm.php
@@ -2,10 +2,14 @@
 
 namespace App\Filament\Resources\Categories\Schemas;
 
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Components\Utilities\Set;
 use Filament\Schemas\Schema;
+use Filament\Tables\Columns\Layout\Split;
 use Illuminate\Support\Str;
 
 class CategoryForm
@@ -14,22 +18,34 @@ class CategoryForm
     {
         return $schema
             ->components([
-                TextInput::make('name')
-                    ->label('Name')
-                    ->live(onBlur: true)
-                    ->afterStateUpdated(function (string $state, callable $set, $get) {
-                        if (blank($get('slug'))) {
+                Section::make([
+                    TextInput::make('name')
+                        ->label('Name')
+                        ->live(onBlur: true)
+                        ->afterStateUpdated(function (Set $set, $state) {
                             $set('slug', Str::slug($state));
-                        }
-                    })
-                    ->required()
-                    ->maxLength(255)
-                    ->minLength(3),
-                TextInput::make('slug')
-                    ->label('Slug')
-                    ->required()
-                    ->maxLength(255)
-                    ->minLength(3),
+                        })
+                        ->required()
+                        ->maxLength(255)
+                        ->minLength(3)
+                        ->columns(1),
+                    TextInput::make('slug')
+                        ->label('Slug')
+                        ->required()
+                        ->maxLength(255)
+                        ->minLength(3)
+                        ->columns(1),
+                    Select::make('status')
+                        ->options([
+                            'draft' => 'Draft',
+                            'reviewing' => 'Reviewing',
+                            'published' => 'Published',
+                        ]),
+                    RichEditor::make('description')
+                        ->columnSpan(2),
+                    FileUpload::make('image')
+                        ->required(),
+                ])->columnSpan(2),
             ]);
     }
 }
